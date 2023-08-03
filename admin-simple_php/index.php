371a12ac32f5c5af68b9012365c1044f6b60039b
Dataram57 <dataram66657@gmail.com>
2023-08-03 22:44:32 +0200

    remote pushing modified docs works

--- admin-simple_php/index.php
+++ admin-simple_php/index.php
@@ -1,5 +1,15 @@
 <?php
+//================================================================
+// This is a 1 single combined PHP API interface
+
+//================================================================
+//Config
+$docsPath = 'docs/';
+$hashesPath = 'hashes.dim';
+
+//================================================================
 //Check password
+
 if(!isset($_POST['password'])){
 	echo "This specific page is admin's page.";
 	return;
@@ -12,8 +22,11 @@ if($password != $passhash){
 	return;
 }
 
+//Request is from the admin
 
-//Custom Dimperpreter
+//================================================================
+//Useful
+
 class Dimperpreter {
     var $code;		//source
 	var $i=0;		//main index
@@ -27,6 +40,7 @@ class Dimperpreter {
 		$this->l = strlen($x);
 	}
 	public function Next(){
+		//
         $this->lastStart = $this->i;
         //
 		$o = $this->i;	//index 2
@@ -63,12 +77,81 @@ class Dimperpreter {
         return substr($this->code,$this->lastStart,$this->lastEnd - $this->lastStart);
     }
     public function ReadRawAfterLast(){
-        return substr($this->code,$this->lastStart);
+        return substr($this->code,$this->lastEnd);
     }
 }
 
-echo shell_exec("python calc_hashes.py");
+function createDirectories($path){
+    if (!is_dir($path))
+        // Create the directory recursively with the necessary permissions
+        // The 'true' argument in mkdir makes it create the directories recursively
+        mkdir($path, 0777, true);
+}
 
+//================================================================
+//Recognize the command
+
+$command = $_POST['command'] ?? '';
+
+//================================================================
+//Modify(docs_path, new_hash, FILE['file'])
+if($command == 'M'){
+	echo 'M: ';
+	$hash = $_POST['hash'] ?? false;
+	if($_FILES['file']){
+		if($hash){
+			//Create directory
+			$relPath = trim($_FILES['file']['full_path']);
+			$targetPath = $docsPath . $relPath;
+			createDirectories(substr($targetPath, 0, strrpos($targetPath, '/')));
+			//Replace
+			move_uploaded_file($_FILES["file"]["tmp_name"], $targetPath);
+			//Update hash
+			$dimp = new Dimperpreter(file_get_contents($hashesPath));
+			$line = $dimp->Next();
+			while($line){
+				if(trim($line[0]) == $relPath){
+					file_put_contents($hashesPath, $dimp->ReadRawBeforeLast() . "\n" . $relPath . ',' . $hash . ';' . $dimp->ReadRawAfterLast());
+					break;
+				}
+				$line = $dimp->Next();
+			}
+			if(!$line)
+				echo "Couldn't find the entry in listed files.";
+			//end
+			else
+				echo "Done.";
+		}
+		else
+			echo "No hash of the file provided.";
+	}
+	else
+		echo "No file uploaded.";
+}
+//================================================================
+//Delete(docs_path)
+else if($command == 'D'){
+	echo 'D';
+}
+//================================================================
+//Add(docs_path, new_hash, FILE['file'])
+else if($command == 'A'){
+	echo 'A';
+}
+//================================================================
+//Bake
+else if($command == 'B'){
+	echo 'B';
+}
+//================================================================
+//?
+else{
+	echo '?';
+}
+
+
+/*
+echo shell_exec("python calc_hashes.py");
 $dimp = new Dimperpreter(file_get_contents('hashes.dim'));
 $line = $dimp->Next();
 while($line){
@@ -76,3 +159,4 @@ while($line){
     echo '<br>';
     $line = $dimp->Next();
 }
+*/
--- remote_push.py
+++ remote_push.py
@@ -1,18 +1,107 @@
+#================================================================
+#Libs
+
 import requests
+import os
 import json
+import subprocess
 
+#================================================================
+#Config
 
-data = {
-    'key1': 'value1',
-    'key2': 'value2'
+docsPath = "docs/"
+config = json.load(open('remote_config.json', 'r'))
+
+#================================================================
+#Functions
+
+def dim_hashes_to_array(txt):
+    arr = []
+    i = -1
+    f = txt.index(';')
+    while f > -1:
+        arr.append(txt[i + 1:f].replace("\n",'').split(','))
+        arr[-1][0] = arr[-1][0].strip()
+        i = f
+        try:
+            f = txt.index(';', i + 1)
+        except:
+            break
+    return arr
+
+def read_file_into_string(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+            return file_content
+    except Exception as e:
+        print(f"Error reading file: {e}")
+        return None
+
+#================================================================
+#Calc hashes and clear screen
+
+#hashes
+subprocess.run(["python", "calc_hashes.py"])
+
+#clear
+if os.system('clear'):
+    os.system('cls')
+print("Started...")
+
+#================================================================
+#Load tables
+
+response = requests.get(config['address'] + '/hashes.dim')
+server_hashes = dim_hashes_to_array(response.text)
+print("Files on the server:", len(server_hashes))
+local_hashes = dim_hashes_to_array(read_file_into_string('hashes.dim'))
+print("Files in workspace:", len(local_hashes))
+
+#================================================================
+#Analyze and Update
+
+post_data = {
+    'password': config['password']
 }
-json_data = json.dumps(data)
+i = -1
+for s in server_hashes:
+    i += 1
+    for l in local_hashes:
+        if s[0] == l[0]:
+            break
+    if s[0] == l[0]:
+        local_hashes.remove(l)
+        server_hashes[i] = False
+        if s[1] != l[1]:
+            #Upload and replace old file
+            print("M:", l)
+            post_data['command'] = 'M'
+            post_data['hash'] = l[1]
+            file = open(docsPath + l[0], 'rb')
+            response = requests.post(config['address'], data = post_data, files = {'file': (l[0], file)})
+            print(response.text)
 
-headers = {
-    'Content-Type': 'application/json',  # Set the appropriate content type if you are sending data
-    'Authorization': 'Bearer YOUR_ACCESS_TOKEN',  # If your API requires authentication
-    # Add any other headers as needed
-}
+while True:
+    try:
+        server_hashes.remove(False)
+    except:
+        break
+for s in server_hashes:
+    #Delete file
+    print("D:", s)
+for l in local_hashes:
+    #Upload new file
+    print("A:", l)
 
-response = requests.get('https://dataram57.com/ip', headers)
-print(response.text)
+#my_array[:index_to_remove] + my_array[index_to_remove + 1:]
+
+
+#================================================================
+
+
+#data = {
+#    'password': config['password']
+#}
+#response = requests.post(config['address'], data)
+#print(response.text)
